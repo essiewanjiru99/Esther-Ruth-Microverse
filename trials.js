a49b2a0d64863e7d387357ee618001f712f28ad3
ruth <bekereruth50@gmail.com>
2023-02-10 19:04:33 -0700

    Varil Adertsing solution

--- trials.js
+++ trials.js
@@ -5,3 +5,13 @@
 
 
 // Ruth's Solution
+function viralAdvertising(n) {
+    // Write your code here
+  let share = 5, like = 0, total = 0;
+  for(let i = 1; i<=n; i++){
+      like = Math.floor(share/2);
+      share = like*3;
+      total +=like;
+  }
+  return total;
+}
